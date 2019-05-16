a8b43c46146cbb2b2d50299304a3d65cfb96dc69
Gonéri Le Bouder <goneri@lebouder.net>
2019-05-16 09:40:47 -0400

    use hostname/FQDN to access the hosts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 vl up
 vl ansible_inventory > ~/vshpere_lab.inventory
-ansible-playbook playbook.yml -i ~/vshpere_lab.inventory
+ansible-playbook playbook.yml -vvvvv -i ~/vshpere_lab.inventory
